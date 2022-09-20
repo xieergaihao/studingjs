187976c0556ab2d54949994d4a1a849926c86f30
xieergai local <xxx@gmail.com>
2022-09-20 20:18:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+let incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(2 * 4 === "8");
+
+const isChecked = true,
+  isClosed = false;
+console.log(isChecked || !isClosed);
